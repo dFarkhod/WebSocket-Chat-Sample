2e92c97901cbae05b1f0df52f4daad2a45019a2f
dFarkhod <dfarkhod@gmail.com>
2019-12-26 11:32:49 +0800

    Initial source code commit
    Birinchi kommit

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
