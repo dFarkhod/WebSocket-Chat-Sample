5dc294f2f43a8459a37bd6d9d9191d878931bbca
dFarkhod <dfarkhod@gmail.com>
2019-12-26 16:01:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', (socket)=>{
+  socket.on('chat message', (msg)=>{
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, function(){
+http.listen(port, ()=>{
   console.log('listening on *:' + port);
 });
